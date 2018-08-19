0383c47a35732420eee862ebe6a52006ddd15fbe
Chad Kinsella <ismisechad@gmail.com>
2018-08-19 19:34:39 +0100

    Add new npm build-prod command to run all commands required for a production build.

--- src/build/build.js
+++ src/build/build.js
@@ -1,9 +1,7 @@
-// const generateHeroData = require('./generateHeroData');
 const generatePlayerData = require('./generatePlayerData');
 
 function generateAppData() {
     console.log('START: Generating app data');
-    // generateHeroData();
     generatePlayerData();
 }
 
